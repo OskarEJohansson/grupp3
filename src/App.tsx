faab4f288a69c9362980a1aa894fc7f4d0c595ea
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-02-27 10:16:21 +0100

    Project created

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>GRUPP 3</p>
+    </>
+  );
+}
+
+export default App;
