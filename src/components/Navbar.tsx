bb27ee9ebb874cf650710df5f9132f705d169693
HampusCastle <Hampuscastle@gmail.com>
2024-03-07 11:29:51 +0100

    More navbar changes with styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,12 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="brand-link">
-          Grupp 3
+        <Link to="/" className="brand-link"><img
+        src='https://trello.com/1/cards/65de1446ad94536b02ad46ce/attachments/65de16933597ddcc58f55a35/previews/65de16943597ddcc58f55aa0/download/loggawhite.png' 
+        alt='Group icon'
+        width="auto"
+        height="100"
+        />
         </Link>
         <button onClick={toggleMenu} className="menu-button">
           {isOpen ? 'Close' : 'Menu'}
@@ -25,7 +29,12 @@ const Navbar = () => {
             <Link to="/about-us">Beverages</Link>
           </li>
           <li>
-            <Link to="/contact-form">Checkout</Link>
+            <Link to="/contact-form"><img
+                src="https://cdn4.vectorstock.com/i/1000x1000/71/93/black-shopping-cart-icon-on-transparent-background-vector-31557193.jpg"
+                alt="Cart"
+                width="40"
+                height="40"
+              /></Link>
           </li>
         </ul>
       </div>
