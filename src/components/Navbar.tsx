1205b58fb5615bfaa02ae0d0328cbfaee82e4263
HampusCastle <Hampuscastle@gmail.com>
2024-03-07 10:17:56 +0100

    Navbar styling changes and functionallity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,36 @@
+import{ useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../App.css'
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <ul>
-                <li><Link to="/">Food</Link></li>
-                <li><Link to="/about-us">Beverages</Link></li>
-                <li><Link to="/contact-form">Navbar-3</Link></li>
-            </ul>
-        </div>
-    );
-}
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="brand-link">
+          Grupp 3
+        </Link>
+        <button onClick={toggleMenu} className="menu-button">
+          {isOpen ? 'Close' : 'Menu'}
+        </button>
+        <ul className={`nav-list ${isOpen ? 'open' : ''}`}>
+          <li>
+            <Link to="/food">Food</Link>
+          </li>
+          <li>
+            <Link to="/about-us">Beverages</Link>
+          </li>
+          <li>
+            <Link to="/contact-form">Checkout</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
-
