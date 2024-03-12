3386113c6bd317674292fd42222ef0a3be46e9e3
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-03-12 15:56:29 +0100

    added foodsections

--- src/pages/FoodPage.tsx
+++ src/pages/FoodPage.tsx
@@ -1,8 +1,25 @@
+import { subscribeWithSelector } from "zustand/middleware";
+import FetchItem from "../components/FetchItem";
+import Subsection from "../components/Subsection";
+import { CardTypes, SubsectionTypes } from "../types";
 
 const FoodPage = () => {
-  return (
-    <div>FoodPage</div>
-  )
-}
+  const foodSections = [
+    { title: "Meat", picture: "picture", _id: 1 },
+    { title: "Veg", picture: "picture", _id: 2 },
+  ];
 
-export default FoodPage
+  return (
+    <div>
+      <div>
+        {foodSections.map((foodSection) => (
+          <Subsection key={foodSection._id} subsectionProps={foodSection} />
+        ))}
+      </div>
+
+      <FetchItem />
+    </div>
+  );
+};
+
+export default FoodPage;
