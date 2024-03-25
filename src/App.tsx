fc9af9d8d433080d4e0e5e93d2d631e18ddbd6c5
HampusCastle <Hampuscastle@gmail.com>
2024-03-25 17:30:16 +0100

    adding an admin view and also changing the routes and navbar to match it

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import FoodRoutes from "./pages/Routes/FoodRoutes/FoodRoutes";
 import BeverageRoutes from "./pages/Routes/BeverageRoutes/BeverageRoutes";
 import UserRoutes from "./pages/Routes/UserRoutes/UserRoutes";
+import AdminRoutes from "./pages/Routes/AdminRoutes/AdminRoutes";
 import Footer from "./components/Footer";
-import CommentsPage from "./pages/CommentsPage";
 
 function App() {
+  const [isAdminView, setIsAdminView] = useState(false);
+
+  const toggleAdminView = () => {
+    setIsAdminView(!isAdminView);
+  };
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar toggleAdminView={toggleAdminView} isAdminView={isAdminView} />
       <Routes>
-        <Route path="/category-page/*" element={<FoodRoutes />} />
-        <Route path="/beverage-page/*" element={<BeverageRoutes />} />
-        <Route path="/*" element={<UserRoutes />} />
-        <Route path="/comments" element={<CommentsPage />} />
+        {isAdminView ? (
+          <Route
+            path="/*"
+            element={
+              <AdminRoutes isAdminView={isAdminView} />
+            }
+          />
+        ) : (
+          <>
+            <Route path="/category-page/*" element={<FoodRoutes />} />
+            <Route path="/beverage-page/*" element={<BeverageRoutes />} />
+            <Route path="/*" element={<UserRoutes />} />
+          </>
+        )}
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,73 +1,33 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      if (scrollPosition > 50) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
+const Navbar = ({ toggleMenu, isOpen, isAdminView, toggleAdminView }) => {
   return (
-    <nav className={`navbar ${isScrolled ? "navbar-scrolled" : ""}`}>
+    <nav className={`navbar ${isOpen ? "navbar-open" : ""}`}>
       <div className="navbar-container">
         <Link to="/" className="brand-link">
-          <img
-            src="https://media.discordapp.net/attachments/1213110126446125117/1214503813306785852/companylogo.png?ex=65f959d1&is=65e6e4d1&hm=75f4b22d314a9030a11ee13c0ad32d1d22fad479cb6cd1f42b43ceab29ad6c8a&=&format=webp&quality=lossless&width=1390&height=464"
-            alt="Group icon"
-            width="auto"
-            height="100"
-          />
         </Link>
         <button onClick={toggleMenu} className="menu-button">
           {isOpen ? "Close" : "Menu"}
         </button>
-        <ul className={`nav-list ${isOpen ? "open" : ""}`}>
+        <ul className="nav-list">
+          {!isAdminView && (
+            <>
+              <li>
+                <Link to="/category-page">Food</Link>
+              </li>
+              <li>
+                <Link to="/beverage-page">Beverages</Link>
+              </li>
+              <li>
+                <Link to="/cart">Cart</Link>
+              </li>
+            </>
+          )}
           <li>
-            <Link to="/category-page">Food</Link>
-          </li>
-          <li>
-            <Link to="/beverage-page">Beverages</Link>
-          </li>
-          <li>
-            <Link to="/cart">
-              <img
-                src="https://t4.ftcdn.net/jpg/01/86/94/37/360_F_186943704_QJkLZaGKmymZuZLPLJrHDMUNpAwuHPjY.jpg"
-                alt="Cart"
-                width="60"
-                height="60"
-              />
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin-page">ADMIN PAGE</Link>
-          </li>
-          <li>
-            <Link to="/add-food-item-page">Add Food</Link>
-          </li>
-          <li>
-            <Link to="/add-comments-page">Add Comment</Link>
-          </li>
-          <li>
-            <Link to="/rating-page">Rating page</Link>
+            <button onClick={toggleAdminView}>
+              {isAdminView ? "Switch to User View" : "Switch to Admin View"}
+            </button>
           </li>
         </ul>
       </div>
