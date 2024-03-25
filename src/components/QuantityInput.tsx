27a3a9afca4fbfc31f6696fc5a125131f67f5556
HampusCastle <Hampuscastle@gmail.com>
2024-03-25 17:11:08 +0100

    added a modal for detailedCard and added a QuantityInput component

---
+++ src/components/QuantityInput.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface QuantityInputProps {
+  value: number;
+  onChange: (newValue: number) => void;
+}
+
+function QuantityInput({ value, onChange }: QuantityInputProps) {
+  return (
+    <input
+      type="number"
+      value={value}
+      onChange={(e) => {
+        const newValue = parseInt(e.target.value);
+        onChange(newValue > 0 ? newValue : 1);
+      }}
+      onClick={(e) => e.stopPropagation()}
+    />
+  );
+}
+
+export default QuantityInput;
