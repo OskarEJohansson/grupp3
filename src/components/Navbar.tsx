33daf55a00ff6b089141202764c2a831c872bbea
HampusCastle <Hampuscastle@gmail.com>
2024-03-25 17:41:41 +0100

    added functionallity for the navbar so user and admin have different views

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,21 +6,41 @@ const Navbar = ({ toggleMenu, isOpen, isAdminView, toggleAdminView }) => {
     <nav className={`navbar ${isOpen ? "navbar-open" : ""}`}>
       <div className="navbar-container">
         <Link to="/" className="brand-link">
+          <img
+            src="https://example.com/logo.png" 
+            alt="Logo"
+            width="100"
+            height="auto"
+          />
         </Link>
         <button onClick={toggleMenu} className="menu-button">
           {isOpen ? "Close" : "Menu"}
         </button>
         <ul className="nav-list">
-          {!isAdminView && (
+          <li>
+            <Link to="/category-page">Food</Link>
+          </li>
+          <li>
+            <Link to="/beverage-page">Beverages</Link>
+          </li>
+          {!isAdminView && ( 
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          )}
+          {isAdminView && (
             <>
               <li>
-                <Link to="/category-page">Food</Link>
+                <Link to="/admin-page">Admin Page</Link>
               </li>
               <li>
-                <Link to="/beverage-page">Beverages</Link>
+                <Link to="/add-food-item-page">Add Food</Link>
               </li>
               <li>
-                <Link to="/cart">Cart</Link>
+                <Link to="/add-comments-page">Add Comment</Link>
+              </li>
+              <li>
+                <Link to="/rating-page">Rating Page</Link>
               </li>
             </>
           )}
--- src/pages/Routes/AdminRoutes/AdminRoutes.tsx
+++ src/pages/Routes/AdminRoutes/AdminRoutes.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import FoodRoutes from "../FoodRoutes/FoodRoutes";
+import ContactForm from "../../../components/ContactForm";
+import AboutUs from "../../AboutUs";
+import AddFoodItemPage from "../../AddFoodItemPage";
+import AdminPage from "../../Admin/AdminPage";
+import CartPage from "../../CartPage";
+import CommentsPage from "../../CommentsPage";
+import HomePage from "../../Home/Homepage";
+import RatingPage from "../../RatingPage";
+import BeverageRoutes from "../BeverageRoutes/BeverageRoutes";
+import Navbar from "../../../components/Navbar";
 
-const AdminRoutes = () => {
+
+const AdminRoutes = ({ isAdminView }) => {
   return (
-    <div>AdminRoutes</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/category-page/*" element={<FoodRoutes />} />
+      <Route path="/beverage-page/*" element={<BeverageRoutes />} />
+      <Route path="/cart" element={isAdminView ? null : <CartPage />} />
+      <Route path="/about-us" element={<AboutUs />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/admin-page" element={<AdminPage />} />
+      <Route path="/add-food-item-page" element={<AddFoodItemPage />} />
+      <Route path="/add-comments-page" element={<CommentsPage />} />
+      <Route path="/contact-form" element={<ContactForm />} />
+      <Route path="/rating-page" element={<RatingPage />} />
+      
+    </Routes>
+  );
+};
 
-export default AdminRoutes
+export default AdminRoutes;
