2b0283cd2f9eb7df13af244b6f3881ac27141c2a
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-03-27 08:17:56 +0100

    restore to dev main

---
+++ src/pages/Beverages/Components/BeverageButton.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import BeverageGlobalState from "../Utilities/BeveragesGlobalState";
+
+const BeverageButton = (category: any) => {
+  const { fetchAlcoholicBeverage, fetchNonAlcoholicBeverage } =
+    BeverageGlobalState();
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    if (category.category === "Alcoholic") {
+      fetchAlcoholicBeverage();
+    } else fetchNonAlcoholicBeverage();
+
+    navigate("/beverage-page/bev");
+  };
+
+  return <button onClick={handleOnClick}>{category.category}</button>;
+};
+
+export default BeverageButton;
