136d8af8eae810922410ac1b1555c45d0b2c8133
MrMcStones <rasmus.ke.nilsson@gmail.com>
2024-04-09 14:24:22 +0200

    route fix

--- src/pages/Navigation/Routes/UserRoutes/UserRoutes.tsx
+++ src/pages/Navigation/Routes/UserRoutes/UserRoutes.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import CartPage from "../../../Cart/CartPage";
 import AboutUs from "../../AboutUs";
 import HomePage from "../../../Home/Homepage";
-import AdminPage from "../../../Admin/AdminAddRecipePage";
+import AdminPage from "../../../Admin/AdminPage";
 import ContactFormPage from "../../../Contact/ContactFormPage";
 
 const UserRoutes = () => {
