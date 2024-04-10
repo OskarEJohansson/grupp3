ae381df10169c488297426b2d5604961d578dc77
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-04-10 09:43:01 +0200

    moved update recipe to admin/see all recipes

--- src/pages/Admin/components/AdminRecipeList.tsx
+++ src/pages/Admin/components/AdminRecipeList.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { RecipeTypes } from "../../../types";
+import UpdateRecipeButton from "../../Update/component/UpdateRecipeButton";
 
 const AdminRecipeList = () => {
   const [recipes, setRecipes] = useState([]);
@@ -39,11 +40,20 @@ const AdminRecipeList = () => {
       {recipes.map((recipe: RecipeTypes, index) => (
         <div key={index} className="bg-white rounded-lg shadow-md p-6 mb-4">
           <h3 className="text-xl font-semibold mb-2">{recipe.title}</h3>
-          <p className="text-gray-700 mb-2">Description: {recipe.description}</p>
-          <p className="text-gray-700 mb-2">Ratings: {recipe.ratings && recipe.ratings.join(", ")}</p>
+          <p className="text-gray-700 mb-2">
+            Description: {recipe.description}
+          </p>
+          <p className="text-gray-700 mb-2">
+            Ratings: {recipe.ratings && recipe.ratings.join(", ")}
+          </p>
           <p className="text-gray-700 mb-2">ImageUrl: {recipe.imageUrl}</p>
-          <p className="text-gray-700 mb-2">Categories: {recipe.categories && recipe.categories.join(", ")}</p>
-          <p className="text-gray-700 mb-2">Instructions: {recipe.instructions && recipe.instructions.join(", ")}</p>
+          <p className="text-gray-700 mb-2">
+            Categories: {recipe.categories && recipe.categories.join(", ")}
+          </p>
+          <p className="text-gray-700 mb-2">
+            Instructions:{" "}
+            {recipe.instructions && recipe.instructions.join(", ")}
+          </p>
           <p className="text-gray-700 mb-2">Ingredients:</p>
           <ul className="list-disc pl-4">
             {recipe.ingredients &&
@@ -53,7 +63,14 @@ const AdminRecipeList = () => {
                 </li>
               ))}
           </ul>
-          <button className="mt-4 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition duration-300 ease-in-out" onClick={() => handleDeleteRecipe(recipe._id)}>Delete Recipe</button>
+
+          <UpdateRecipeButton article={recipe} />
+          <button
+            className="mt-4 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition duration-300 ease-in-out"
+            onClick={() => handleDeleteRecipe(recipe._id)}
+          >
+            Delete Recipe
+          </button>
         </div>
       ))}
     </div>
