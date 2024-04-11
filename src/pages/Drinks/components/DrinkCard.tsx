f8bc47fd9cf70bf6dbc734e1dc6c48172f9e5a62
HampusCastle <Hampuscastle@gmail.com>
2024-04-11 09:19:41 +0200

    added styling to search and drinkcard

--- src/pages/Drinks/components/DrinkCard.tsx
+++ src/pages/Drinks/components/DrinkCard.tsx
@@ -6,13 +6,11 @@ import DrinkDetailedCard from "./DrinkDetailedCard";
 import { DrinkTypes } from "../../../types";
 
 const DrinkCardPage = () => {
-  const { drinkList, fetchAlcoholicDrink, fetchNonAlcoholicDrink, fetchDrink } =
-    DrinkGlobalState();
+  const { drinkList, fetchAlcoholicDrink, fetchNonAlcoholicDrink, fetchDrink } = DrinkGlobalState();
   const { category } = useParams();
   const [showModal, setShowModal] = useState(false);
 
   const handleOnClick = (drink: DrinkTypes) => {
-    console.log("Handle on click to detailed card", drink);
     fetchDrink(drink.idDrink);
     setShowModal(true);
   };
@@ -27,23 +25,27 @@ const DrinkCardPage = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-bg-gradient-start to-bg-gradient-end font-serif flex flex-col items-center justify-center pt-20">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-8 px-4">
         {drinkList.map((drink, index) => (
           <div
             key={index}
-            className="bg-white rounded-lg shadow-lg overflow-hidden border border-gray-300 cursor-pointer flex flex-col"
+            className="bg-white rounded-lg shadow-md overflow-hidden border-double border-4 border-gray-300 cursor-pointer hover:shadow-lg transition duration-300 relative"
             onClick={() => handleOnClick(drink)}
           >
-            <img
-              src={drink.strDrinkThumb}
-              alt={drink.strDrink}
-              className="w-full"
-            />
-            <div className="p-4 flex-grow">
-              <h2 className="text-xl font-bold mb-2">{drink.strDrink}</h2>
+            <div className="relative">
+              <img
+                src={drink.strDrinkThumb}
+                alt={drink.strDrink}
+                className="w-full h-64 object-cover"
+              />
+              <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-50"></div>
             </div>
-            <div className="justify-end">
-              <AddToCartButton article={drink} />
+            <div className="p-4 absolute bottom-0 left-0 w-full text-white">
+              <h2 className="text-lg font-semibold mb-2">{drink.strDrink}</h2>
+              <p className="text-gray-200 mb-5">{category}</p>
+              <div>
+                <AddToCartButton article={drink} />
+              </div>
             </div>
           </div>
         ))}
--- src/pages/Navigation/Searchbar/components/SearchbarButton.tsx
+++ src/pages/Navigation/Searchbar/components/SearchbarButton.tsx
@@ -2,20 +2,25 @@ import SearchbarGlobalState from "../utils/SearchbarGlobalState";
 import { useNavigate } from "react-router-dom";
 
 const SearchbarButton = () => {
-  const { searchInput, allRecipes, fetchDrinkByName, filterRecipes } =
-    SearchbarGlobalState();
+  const { searchInput, allRecipes, fetchDrinkByName, filterRecipes, setSearchbarInput } = SearchbarGlobalState();
   const navigate = useNavigate();
 
   const handleOnClick = () => {
     fetchDrinkByName(searchInput);
     filterRecipes(searchInput, allRecipes);
     navigate("/search-result");
+    setSearchbarInput("");
   };
+
   return (
     <div>
-      <button onClick={handleOnClick}>Search</button>
+      <button className="bg-yellow-500 text-white px-2 py-1 rounded" onClick={handleOnClick}>
+        Search
+      </button>
     </div>
   );
 };
 
 export default SearchbarButton;
+
+
