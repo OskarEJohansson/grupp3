f1aebeefa0d14bec6bbc71fed3614626b76b2342
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-04-11 10:17:30 +0200

    Added real time rating update

--- src/pages/Rating/utils/RatingGlobalState.tsx
+++ src/pages/Rating/utils/RatingGlobalState.tsx
@@ -4,14 +4,17 @@ import { create } from "zustand";
 export interface RatingGlobalStateInterface {
   ratings: [];
   recipeId: string | undefined;
+  averageRating: number;
 
   setRecipeId: (id: string) => void;
   postRating: (id: string, rating: object) => void;
+  fetchAverageRating: (id: string) => void;
 }
 
 const RatingGlobalState = create<RatingGlobalStateInterface>((set) => ({
   ratings: [],
   recipeId: undefined,
+  averageRating: 0,
 
   setRecipeId: (id: string) => {
     set(() => ({
@@ -33,6 +36,23 @@ const RatingGlobalState = create<RatingGlobalStateInterface>((set) => ({
       } else console.log("Post Rating Failed", response.status);
     } catch (error) {
       console.log("Post Rating Catch Error", error);
+    }
+  },
+
+  fetchAverageRating: async (id: string) => {
+    try {
+      const response = await axios.get(
+        `https://sti-java-grupp3-mzba2l.reky.se/recipes/${id}`
+      );
+
+      const { avgRating } = response.data;
+      console.log("Average Rating:", avgRating);
+
+      set(() => ({
+        averageRating: avgRating !== undefined ? avgRating : 0,
+      }));
+    } catch (error) {
+      console.error("Error fetching average rating:", error);
     }
   },
 }));
