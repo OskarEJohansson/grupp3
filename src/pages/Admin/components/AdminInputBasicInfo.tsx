153df4161fc6b2c89b28a5a75af1a9c852bf276e
MrMcStones <rasmus.ke.nilsson@gmail.com>
2024-04-12 11:07:32 +0200

    placeholders for add recipe form

--- src/pages/Admin/components/AdminInputBasicInfo.tsx
+++ src/pages/Admin/components/AdminInputBasicInfo.tsx
@@ -34,6 +34,7 @@ const AdminInputBasicInfo = ({ inputLabel }: InputFormBaseProps) => {
         <input
           type="text"
           id={inputId}
+          placeholder={inputId}
           onChange={handleOnChange}
           className="block w-full bg-white border border-gray-300 rounded-md px-3 py-2 mt-1 focus:outline-none focus:border-blue-500"
           required
